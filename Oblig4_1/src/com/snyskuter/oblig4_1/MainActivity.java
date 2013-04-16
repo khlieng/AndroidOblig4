34989f40dd1db5b2d2bbf629ea591959cf59eb9b
martinbang <elexer@gmail.com>
2013-04-16 20:35:02 +0200

    Fikset stop og start service i action bar. den fungerer. Har også aget en alert dialog til instillinger. her skal man definere selv, og kan legge inn tekstfelter. blablabla

--- Oblig4_1/src/com/snyskuter/oblig4_1/MainActivity.java
+++ Oblig4_1/src/com/snyskuter/oblig4_1/MainActivity.java
@@ -1,15 +1,25 @@
 package com.snyskuter.oblig4_1;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private Button saveButtonAlertDialog;
+	final Context context = this;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -52,18 +62,53 @@ public class MainActivity extends Activity {
 			break;
 		case R.id.Instillinger:
 			Toast.makeText(this, "Instillinger", Toast.LENGTH_SHORT).show();
+			initAlertDialog();
 			break;
 		case R.id.start:
+			Log.e("Service STARTED", "Service Started");
 			Toast.makeText(this, "Starting service", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(this,TemperatureService.class);
+			startService(intent);
 			break;
 		case R.id.stop:
+			Log.e("Service STOP", "Service Stopped");
 			Toast.makeText(this, "Service stopped", Toast.LENGTH_SHORT).show();
+			Intent inte = new Intent(this,TemperatureService.class);
+			stopService(inte);
 			break;
 
 		default:
 			break;
 		}
 		return true;
+	}
+	
+	public void initAlertDialog(){
+		
+		final Dialog dialog = new Dialog(context);
+		dialog.setContentView(R.layout.properties_custom);
+		dialog.setTitle("Instillinger");
+		
+
+		// set the custom dialog components - text, image and button
+		TextView text = (TextView) dialog.findViewById(R.id.textAlertDialog);
+		text.setText("Ganster!");
+		
+		
+		Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonSaveAlert);
+		
+		
+					dialogButton.setOnClickListener(new OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							Log.e("Saving button clicked", "Click");
+							Toast.makeText(getApplicationContext(), "Saving.....", Toast.LENGTH_SHORT).show();
+							
+							dialog.dismiss();
+						}
+					});
+		 
+					dialog.show();
 	}
 
 }//end activiy
